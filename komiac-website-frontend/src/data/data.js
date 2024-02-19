8c11ed239c2b9efb1fd51f9357118d960aff7abf
L1oid <lloidtm@gmail.com>
2024-02-19 14:34:14 +0700

    upd data

--- komiac-website-frontend/src/data/data.js
+++ komiac-website-frontend/src/data/data.js
@@ -16,8 +16,8 @@ export let data_medical_organizations = [
             "id": 487,
             "vipnetSettings": {
                 "id": 469,
-                "name": "название сети",
-                "address": "ip address"
+                "name": "Сеть 1",
+                "address": "192.168.0.1"
             },
             "pathToDb": "путь до БД",
             "login": "логин",
@@ -67,8 +67,8 @@ export let data_medical_organizations = [
         "name": "Подразделение 1",
         "address": "Адрес подразделения",
         "oID": "OID подразделения",
-        "territoryCode": 123,
-        "divisionCode": 321,
+        "territoryCode": 1,
+        "divisionCode": 9,
         "divisionType": "Hospital"
     },
     {
@@ -139,8 +139,512 @@ export let data_medical_organizations = [
         "name": "Подразделение 2",
         "address": "Адрес подразделения",
         "oID": "OID подразделения",
-        "territoryCode": 456,
-        "divisionCode": 654,
+        "territoryCode": 2,
+        "divisionCode": 8,
+        "divisionType": "Hospital"
+    },
+    {
+        "id": "0007ec24-a7d8-4b80-bfb5-65026888a15a",
+        "medicalOrganizationId": "9288fdbb-56d3-461f-970d-4d3c95eacea4",
+        "scheduleOptions": {
+            "id": 100,
+            "patientSearch": "PolisOMS",
+            "isOnlyAttachment": true,
+            "displayTickets": "Online",
+            "daysRange": 14,
+            "isDoctorArea": false,
+            "isAllowCreateCard": false,
+            "isHasOnlyOneTicketToSpeciality": true
+        },
+        "dbSettings": {
+            "id": 487,
+            "vipnetSettings": {
+                "id": 469,
+                "name": "название сети",
+                "address": "ip address"
+            },
+            "pathToDb": "путь до БД",
+            "login": "логин",
+            "password": "пароль",
+            "provider": "Система 1"
+        },
+        "useModules": {
+            "id": 488,
+            "isUseHomeCall": false,
+            "isUseDispanserization": false,
+            "isUseVaccination": false,
+            "isUseAppointment": true,
+            "isUseDocumentsCallback": true,
+            "isUseNativeSite": false,
+            "isUseDispanserizationNotification": false
+        },
+        "contacts": [
+            {
+                "id": 738,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "contactType": "Phone",
+                "value": "+79009009090"
+            },
+            {
+                "id": 739,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "contactType": "Email",
+                "value": "email@gmail.com"
+            }
+        ],
+        "externalIds": [
+            {
+                "id": 489,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "externalSystem": "Система 3",
+                "value": "1000"
+            }
+        ],
+        "isApproved": true,
+        "isActive": true,
+        "name": "Подразделение 3",
+        "address": "Адрес подразделения",
+        "oID": "OID подразделения",
+        "territoryCode": 3,
+        "divisionCode": 7,
+        "divisionType": "Hospital"
+    },
+    {
+        "id": "0007ec24-a7d8-4b80-bfb5-65026888a16a",
+        "medicalOrganizationId": "9288fdbb-56d3-461f-970d-4d3c95eacea4",
+        "scheduleOptions": {
+            "id": 100,
+            "patientSearch": "PolisOMS",
+            "isOnlyAttachment": true,
+            "displayTickets": "Online",
+            "daysRange": 14,
+            "isDoctorArea": false,
+            "isAllowCreateCard": false,
+            "isHasOnlyOneTicketToSpeciality": true
+        },
+        "dbSettings": {
+            "id": 487,
+            "vipnetSettings": {
+                "id": 469,
+                "name": "название сети",
+                "address": "ip address"
+            },
+            "pathToDb": "путь до БД",
+            "login": "логин",
+            "password": "пароль",
+            "provider": "Система 1"
+        },
+        "useModules": {
+            "id": 488,
+            "isUseHomeCall": false,
+            "isUseDispanserization": false,
+            "isUseVaccination": false,
+            "isUseAppointment": true,
+            "isUseDocumentsCallback": true,
+            "isUseNativeSite": false,
+            "isUseDispanserizationNotification": false
+        },
+        "contacts": [
+            {
+                "id": 738,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "contactType": "Phone",
+                "value": "+79009009090"
+            },
+            {
+                "id": 739,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "contactType": "Email",
+                "value": "email@gmail.com"
+            }
+        ],
+        "externalIds": [
+            {
+                "id": 489,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "externalSystem": "Система 3",
+                "value": "1000"
+            }
+        ],
+        "isApproved": true,
+        "isActive": true,
+        "name": "Подразделение 4",
+        "address": "Адрес подразделения",
+        "oID": "OID подразделения",
+        "territoryCode": 4,
+        "divisionCode": 6,
+        "divisionType": "Hospital"
+    },
+    {
+        "id": "0007ec24-a7d8-4b80-bfb5-65026888a17a",
+        "medicalOrganizationId": "9288fdbb-56d3-461f-970d-4d3c95eacea4",
+        "scheduleOptions": {
+            "id": 100,
+            "patientSearch": "PolisOMS",
+            "isOnlyAttachment": true,
+            "displayTickets": "Online",
+            "daysRange": 14,
+            "isDoctorArea": false,
+            "isAllowCreateCard": false,
+            "isHasOnlyOneTicketToSpeciality": true
+        },
+        "dbSettings": {
+            "id": 487,
+            "vipnetSettings": {
+                "id": 469,
+                "name": "название сети",
+                "address": "ip address"
+            },
+            "pathToDb": "путь до БД",
+            "login": "логин",
+            "password": "пароль",
+            "provider": "Система 1"
+        },
+        "useModules": {
+            "id": 488,
+            "isUseHomeCall": false,
+            "isUseDispanserization": false,
+            "isUseVaccination": false,
+            "isUseAppointment": true,
+            "isUseDocumentsCallback": true,
+            "isUseNativeSite": false,
+            "isUseDispanserizationNotification": false
+        },
+        "contacts": [
+            {
+                "id": 738,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "contactType": "Phone",
+                "value": "+79009009090"
+            },
+            {
+                "id": 739,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "contactType": "Email",
+                "value": "email@gmail.com"
+            }
+        ],
+        "externalIds": [
+            {
+                "id": 489,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "externalSystem": "Система 3",
+                "value": "1000"
+            }
+        ],
+        "isApproved": true,
+        "isActive": true,
+        "name": "Подразделение 5",
+        "address": "Адрес подразделения",
+        "oID": "OID подразделения",
+        "territoryCode": 5,
+        "divisionCode": 5,
+        "divisionType": "Hospital"
+    },
+    {
+        "id": "0007ec24-a7d8-4b80-bfb5-65026888a18a",
+        "medicalOrganizationId": "9288fdbb-56d3-461f-970d-4d3c95eacea4",
+        "scheduleOptions": {
+            "id": 100,
+            "patientSearch": "PolisOMS",
+            "isOnlyAttachment": true,
+            "displayTickets": "Online",
+            "daysRange": 14,
+            "isDoctorArea": false,
+            "isAllowCreateCard": false,
+            "isHasOnlyOneTicketToSpeciality": true
+        },
+        "dbSettings": {
+            "id": 487,
+            "vipnetSettings": {
+                "id": 469,
+                "name": "название сети",
+                "address": "ip address"
+            },
+            "pathToDb": "путь до БД",
+            "login": "логин",
+            "password": "пароль",
+            "provider": "Система 1"
+        },
+        "useModules": {
+            "id": 488,
+            "isUseHomeCall": false,
+            "isUseDispanserization": false,
+            "isUseVaccination": false,
+            "isUseAppointment": true,
+            "isUseDocumentsCallback": true,
+            "isUseNativeSite": false,
+            "isUseDispanserizationNotification": false
+        },
+        "contacts": [
+            {
+                "id": 738,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "contactType": "Phone",
+                "value": "+79009009090"
+            },
+            {
+                "id": 739,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "contactType": "Email",
+                "value": "email@gmail.com"
+            }
+        ],
+        "externalIds": [
+            {
+                "id": 489,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "externalSystem": "Система 3",
+                "value": "1000"
+            }
+        ],
+        "isApproved": true,
+        "isActive": true,
+        "name": "Подразделение 6",
+        "address": "Адрес подразделения",
+        "oID": "OID подразделения",
+        "territoryCode": 6,
+        "divisionCode": 4,
+        "divisionType": "Hospital"
+    },
+    {
+        "id": "0007ec24-a7d8-4b80-bfb5-65026888a19a",
+        "medicalOrganizationId": "9288fdbb-56d3-461f-970d-4d3c95eacea4",
+        "scheduleOptions": {
+            "id": 100,
+            "patientSearch": "PolisOMS",
+            "isOnlyAttachment": true,
+            "displayTickets": "Online",
+            "daysRange": 14,
+            "isDoctorArea": false,
+            "isAllowCreateCard": false,
+            "isHasOnlyOneTicketToSpeciality": true
+        },
+        "dbSettings": {
+            "id": 487,
+            "vipnetSettings": {
+                "id": 469,
+                "name": "название сети",
+                "address": "ip address"
+            },
+            "pathToDb": "путь до БД",
+            "login": "логин",
+            "password": "пароль",
+            "provider": "Система 1"
+        },
+        "useModules": {
+            "id": 488,
+            "isUseHomeCall": false,
+            "isUseDispanserization": false,
+            "isUseVaccination": false,
+            "isUseAppointment": true,
+            "isUseDocumentsCallback": true,
+            "isUseNativeSite": false,
+            "isUseDispanserizationNotification": false
+        },
+        "contacts": [
+            {
+                "id": 738,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "contactType": "Phone",
+                "value": "+79009009090"
+            },
+            {
+                "id": 739,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "contactType": "Email",
+                "value": "email@gmail.com"
+            }
+        ],
+        "externalIds": [
+            {
+                "id": 489,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "externalSystem": "Система 3",
+                "value": "1000"
+            }
+        ],
+        "isApproved": true,
+        "isActive": true,
+        "name": "Подразделение 7",
+        "address": "Адрес подразделения",
+        "oID": "OID подразделения",
+        "territoryCode": 7,
+        "divisionCode": 3,
+        "divisionType": "Hospital"
+    },
+    {
+        "id": "0007ec24-a7d8-4b80-bfb5-65026888a20a",
+        "medicalOrganizationId": "9288fdbb-56d3-461f-970d-4d3c95eacea4",
+        "scheduleOptions": {
+            "id": 100,
+            "patientSearch": "PolisOMS",
+            "isOnlyAttachment": true,
+            "displayTickets": "Online",
+            "daysRange": 14,
+            "isDoctorArea": false,
+            "isAllowCreateCard": false,
+            "isHasOnlyOneTicketToSpeciality": true
+        },
+        "dbSettings": {
+            "id": 487,
+            "vipnetSettings": {
+                "id": 469,
+                "name": "название сети",
+                "address": "ip address"
+            },
+            "pathToDb": "путь до БД",
+            "login": "логин",
+            "password": "пароль",
+            "provider": "Система 1"
+        },
+        "useModules": {
+            "id": 488,
+            "isUseHomeCall": false,
+            "isUseDispanserization": false,
+            "isUseVaccination": false,
+            "isUseAppointment": true,
+            "isUseDocumentsCallback": true,
+            "isUseNativeSite": false,
+            "isUseDispanserizationNotification": false
+        },
+        "contacts": [
+            {
+                "id": 738,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "contactType": "Phone",
+                "value": "+79009009090"
+            },
+            {
+                "id": 739,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "contactType": "Email",
+                "value": "email@gmail.com"
+            }
+        ],
+        "externalIds": [
+            {
+                "id": 489,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "externalSystem": "Система 3",
+                "value": "1000"
+            }
+        ],
+        "isApproved": true,
+        "isActive": true,
+        "name": "Подразделение 8",
+        "address": "Адрес подразделения",
+        "oID": "OID подразделения",
+        "territoryCode": 8,
+        "divisionCode": 2,
+        "divisionType": "Hospital"
+    },
+    {
+        "id": "0007ec24-a7d8-4b80-bfb5-65026888a21a",
+        "medicalOrganizationId": "9288fdbb-56d3-461f-970d-4d3c95eacea4",
+        "scheduleOptions": {
+            "id": 100,
+            "patientSearch": "PolisOMS",
+            "isOnlyAttachment": true,
+            "displayTickets": "Online",
+            "daysRange": 14,
+            "isDoctorArea": false,
+            "isAllowCreateCard": false,
+            "isHasOnlyOneTicketToSpeciality": true
+        },
+        "dbSettings": {
+            "id": 487,
+            "vipnetSettings": {
+                "id": 469,
+                "name": "название сети",
+                "address": "ip address"
+            },
+            "pathToDb": "путь до БД",
+            "login": "логин",
+            "password": "пароль",
+            "provider": "Система 1"
+        },
+        "useModules": {
+            "id": 488,
+            "isUseHomeCall": false,
+            "isUseDispanserization": false,
+            "isUseVaccination": false,
+            "isUseAppointment": true,
+            "isUseDocumentsCallback": true,
+            "isUseNativeSite": false,
+            "isUseDispanserizationNotification": false
+        },
+        "contacts": [
+            {
+                "id": 738,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "contactType": "Phone",
+                "value": "+79009009090"
+            },
+            {
+                "id": 739,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "contactType": "Email",
+                "value": "email@gmail.com"
+            }
+        ],
+        "externalIds": [
+            {
+                "id": 489,
+                "medicalOrganizationId": null,
+                "divisionId": "0007ec24-a7d8-4b80-bfb5-65026888a13a",
+                "isDeleted": false,
+                "externalSystem": "Система 3",
+                "value": "1000"
+            }
+        ],
+        "isApproved": true,
+        "isActive": true,
+        "name": "Подразделение 9",
+        "address": "Адрес подразделения",
+        "oID": "OID подразделения",
+        "territoryCode": 9,
+        "divisionCode": 1,
         "divisionType": "Hospital"
     }
 ]
